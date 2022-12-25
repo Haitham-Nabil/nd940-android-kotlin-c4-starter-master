f00f8e3d2270e05d6baf1326bc80c6e052e8d632
Haitham-Nabil <120406718+Haitham-Nabil@users.noreply.github.com>
2022-12-25 19:27:22 +0200

    Complete first androidtest Dao

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -5,6 +5,8 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 //import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 
 import org.junit.Before;
@@ -26,5 +28,41 @@ import org.junit.Test
 class RemindersDaoTest {
 
 //    TODO: Add testing implementation to the RemindersDao.kt
+@get:Rule
+var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var database: RemindersDatabase
+    val list1 = ReminderDTO("Computer Mall", "ask for labtop price", "tanta", 30.23549 , 30.6548)
+    val list2 = ReminderDTO("brother house", "say hello to friend", "tanta", 30.23545,30.5628)
+    val list3 = ReminderDataItem("friend house", "say hello to friend", "tanta", 30.23545,30.5628)
+
+    @Before
+    fun initDb() {
+        // Using an in-memory database so that the information stored here disappears when the
+        // process is killed.
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun insertReminderAndGetById() = runBlockingTest {
+
+        database.reminderDao().saveReminder(list1)
+
+        val result = database.reminderDao().getReminderById(list1.id)
+
+
+        assertThat(result as ReminderDTO, notNullValue())
+        assertThat(result.id, `is`(list1.id))
+        assertThat(result.title, `is`(list1.title))
+        assertThat(result.description, `is`(list1.description))
+        assertThat(result.location, `is`(list1.location))
+        assertThat(result.latitude, `is`(list1.latitude))
+        assertThat(result.longitude, `is`(list1.longitude))
+    }
 }
